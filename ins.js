b17ce73964bc4ec954220fb34433753f3fbdb47b
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-03-01 23:02:43 +0530

    cg

---
+++ ins.js
@@ -0,0 +1,16 @@
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let url = "http://universities.hipolabs.com/search?country=india";
+btn.addEventListener("click",async ()=>{
+    
+    let ip = await inp();
+    
+})
+async function inp(){
+    
+    let datas = await axios.get(url);
+    let state = input.value;
+    console.log(state);
+    return datas.data.state-province;
+}
